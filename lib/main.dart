bb54cbd8332d0cdd35396f84902d39b975642eff
Cesar-JCHV <a12677@universidad-une.com>
2023-09-24 23:18:29 -0600

    Se agrega el proyecto

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:pokedex/bloc/pokemon_event.dart';
+import 'package:pokedex/pokedex_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: Theme.of(context).copyWith(primaryColor: Colors.red, accentColor: Colors.redAccent),
+      home: MultiBlocProvider(
+        providers: [BlocProvider(create: (context) => PokemonBloc()..add(PokemonPageRequest(page: 0)))], child: PokedexView()),
+    );
+  }
+}
